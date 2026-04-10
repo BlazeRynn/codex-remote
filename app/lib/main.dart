7ecd71ebb6b071300588e13f4f5e6e1def12d609
亮 <369139415@qq.com>
2026-04-10 21:23:27 +0800

    Add mobile screen wake lock

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/widgets.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'app/app.dart';
 import 'services/app_preferences_store.dart';
 import 'services/bridge_config_store.dart';
 import 'services/local_notification_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid || Platform.isIOS) {
+    await WakelockPlus.enable();
+  }
   runApp(
     CodexMobileApp(
       configStore: SharedPrefsBridgeConfigStore(),
